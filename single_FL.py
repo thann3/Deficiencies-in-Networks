d1ab9f1d7e8acfb049ba515d679c800969d4fc72
Ethan TS. Liu <87835921+thann3@users.noreply.github.com>
2023-07-30 14:09:05 -0700

    Update single_FL.py

--- single_FL.py
+++ single_FL.py
@@ -4,100 +4,103 @@
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models, datasets
 
-# Set random seed for reproducibility
-np.random.seed(42)
+#Ensure reproducibility
+seed = 42
+np.random.seed(seed)
+tf.random.set_seed(seed)
 
-# Define the negative log likelihood cost function
+num_rounds = 10000
+probability_to_disconnect = 0
+learning_rate = 0.01
+
+# Define the cost function: (x_i - c_i)^2
 def cost_function(x, c):
-    return tf.keras.losses.sparse_categorical_crossentropy(c, x)
+    return tf.square(x - c)
 
 # Define the standard FedAvg algorithm
 def federated_averaging(models):
     if len(models) > 0:
-        averaged_weights = []
-        for layer_weights in zip(*[model.get_weights() for model in models]):
-            averaged_layer_weights = tf.reduce_mean(layer_weights, axis=0)
-            averaged_weights.append(averaged_layer_weights.numpy())
-        return averaged_weights
+        return np.mean(models, axis=0)
     else:
         return None
 
+# Initialize nodes and server
+c_values = np.array([1, 2, 3, 4, 5], dtype=np.float32)
+
+num_nodes = 5
+nodes = [tf.Variable(np.random.randn(1).astype(np.float32)) for _ in range(num_nodes)]
+global_model = tf.Variable(np.random.randn(1).astype(np.float32))
+
 # Function to simulate a round of asynchronous federated learning
-def train_one_round(global_model, nodes, data_by_node, learning_rate, probability_to_disconnect, straggler_counts):
+def train_one_round(global_model, nodes, c_values, learning_rate, probability_to_disconnect, straggler_counts):
     # Perform updates for each node
-    for node_idx, node in enumerate(nodes):
+    disconnected_nodes = []  # Store disconnected nodes
+    for node in nodes:
         if np.random.rand() > probability_to_disconnect:
-            x_train, y_train = data_by_node[node_idx]
             with tf.GradientTape() as tape:
-                logits = node(x_train)
-                loss = tf.reduce_mean(cost_function(logits, y_train))
-            gradients = tape.gradient(loss, node.trainable_variables)
-            node.optimizer.apply_gradients(zip(gradients, node.trainable_variables))
+                cost = cost_function(node, c_values[nodes.index(node)])
+            gradients = tape.gradient(cost, node)
+            node.assign_sub(learning_rate * gradients)
         else:
-            # Node is a straggler, increase the count in the dictionary
-            straggler_counts[node_idx] += 1
+            # Node is disconnected, add to the disconnected_nodes list
+            disconnected_nodes.append(node)
+            # Increment the straggler count for the disconnected node
+            straggler_counts[nodes.index(node)] += 1  # Update the straggler count
 
     # Aggregate models using FedAvg
-    models = [node for node_idx, node in enumerate(nodes) if np.random.rand() > probability_to_disconnect]
-    aggregated_model_weights = federated_averaging(models)
-    if aggregated_model_weights is not None:
-        global_model.set_weights(aggregated_model_weights)
+    models = [node.numpy() for node in nodes if np.random.rand() > probability_to_disconnect]
+    aggregated_model = federated_averaging(models)
+    if aggregated_model is not None:
+        global_model.assign(aggregated_model)
 
     # Send the updated global model back to all nodes
     for node in nodes:
         if np.random.rand() > probability_to_disconnect:
-            node.set_weights(global_model.get_weights())
+            node.assign(global_model)
+
+    # Perform gradient steps on disconnected nodes using the global model after aggregation
+    for disconnected_node in disconnected_nodes:
+        if np.random.rand() > probability_to_disconnect:
+            with tf.GradientTape() as tape:
+                cost = cost_function(disconnected_node, c_values[nodes.index(disconnected_node)])
+            gradients = tape.gradient(cost, disconnected_node)
+            disconnected_node.assign_sub(learning_rate * gradients)
 
 # Function to run multiple rounds of asynchronous federated learning
 def run_federated_learning(num_rounds, probability_to_disconnect, learning_rate):
-    # Download and preprocess the MNIST dataset
-    (x_train, y_train), _ = datasets.mnist.load_data()
-    x_train = x_train / 255.0  # Normalize pixel values to [0, 1]
-    x_train = x_train[..., np.newaxis]  # Add a channel dimension for CNN input
-    num_nodes = 5  # Number of nodes participating in federated learning
-    data_per_node = len(x_train) // num_nodes
-    data_by_node = [(x_train[i * data_per_node: (i + 1) * data_per_node],
-                     y_train[i * data_per_node: (i + 1) * data_per_node])
-                    for i in range(num_nodes)]
-
-    # Create and compile the global model
-    global_model = models.Sequential([
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        layers.MaxPooling2D((2, 2)),
-        layers.Conv2D(64, (3, 3), activation='relu'),
-        layers.MaxPooling2D((2, 2)),
-        layers.Flatten(),
-        layers.Dense(64, activation='relu'),
-        layers.Dense(10, activation='softmax')
-    ])
-    global_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-    # Create and compile individual nodes with separate data
-    nodes = [models.clone_model(global_model) for _ in range(num_nodes)]
-    for node in nodes:
-        node.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
     # Initialize the dictionary to keep track of straggler counts for each node
     straggler_counts = {i: 0 for i in range(num_nodes)}
 
-    # Print the initial accuracy of the nodes
-    initial_accs = [node.evaluate(data_by_node[i][0], data_by_node[i][1], verbose=0)[1] for i, node in enumerate(nodes)]
-    print("Initial accuracies:", initial_accs)
+    # Print the initial estimates of the nodes
+    initial_x_values = [node.numpy()[0] for node in nodes]
+    print("Initial x values:", initial_x_values)
 
     for round in range(num_rounds):
-        train_one_round(global_model, nodes, data_by_node, learning_rate, probability_to_disconnect, straggler_counts)
+        train_one_round(global_model, nodes, c_values, learning_rate, probability_to_disconnect, straggler_counts)
 
-    # Get the final accuracy of the nodes and the accuracy of the global model
-    final_accs = [node.evaluate(data_by_node[i][0], data_by_node[i][1], verbose=0)[1] for i, node in enumerate(nodes)]
-    global_acc = global_model.evaluate(x_train, y_train, verbose=0)[1]
+    # Get the final x values and the optimal solution x*
+    final_x_values = [node.numpy()[0] for node in nodes]
+    optimal_x = 3
 
-    print("Final accuracies:", final_accs)
-    print("Global model accuracy:", global_acc)
-    print("Average accuracy of nodes:", np.mean(final_accs))
+    print("Final x values:", final_x_values)
 
-    # Print the straggler counts for each node
+    mean_x = 0
+    for i in final_x_values:
+        mean_x += i
+    mean_x /= len(final_x_values)
+
+    print("Mean x value:", mean_x)
+
+    print("Optimal solution:", optimal_x)
+
+    # Calculate the sum of the L1 norms of the final values with respect to x=3
+    l1_norm_sum = np.sum([abs(final_x - optimal_x) for final_x in final_x_values])
+
+    # Print the sum of the L1 norms of the final values
+    print("L1 norm:", l1_norm_sum)
+
+    # Print the straggler counts for each node after the rounds are completed
     print("Straggler counts:", straggler_counts)
 
-run_federated_learning(num_rounds=100, probability_to_disconnect=0.1, learning_rate=0.01)
+run_federated_learning(num_rounds, probability_to_disconnect, learning_rate)
